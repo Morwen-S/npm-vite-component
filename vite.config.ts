31360acd74bc4e00d01ad749f2cdbe614ca445b1
Svetlana Butina <s.butina@geeksoft.kz>
2022-08-26 14:07:57 +0600

    change

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import vue from '@vitejs/plugin-vue2'
+import { createVuePlugin } from 'vite-plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [createVuePlugin()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
