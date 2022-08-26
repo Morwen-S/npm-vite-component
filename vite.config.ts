e74e294d531b8f82e7c461877f814659f38cb426
Svetlana Butina <s.butina@geeksoft.kz>
2022-08-26 13:14:15 +0600

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,27 +5,27 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    dedupe: ['vue']
-  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'npm-vite-component',
-      fileName: `npm-vite-component`,
+      fileName: (format) => `npm-vite-component.${format}.js`,
       // the proper extensions will be added
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: ['vue'],
+      resolve: {
+        dedupe: ['vue']
+      },
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: "vue"
+          vue: "Vue"
         },
       }
-    }
+    },
   }
 })
