9c6d82a222347f7436c67c94592bf867ef58913d
Svetlana Butina <s.butina@geeksoft.kz>
2022-08-26 16:44:34 +0600

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,42 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
-import vue from '@vitejs/plugin-vue2'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/main.ts'),
-      name: 'npm-vite-component',
-      fileName: 'npm-vite-component',
-      // the proper extensions will be added
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      resolve: {
-        dedupe: ['vue']
+import {default as vue2} from '@vitejs/plugin-vue2';
+import {default as vue3} from '@vitejs/plugin-vue';
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  const VUE_VERSION = process.env.VITE_VUE_VERSIONS;
+
+  console.log(mode, VUE_VERSION);
+
+  const vuePlugin = VUE_VERSION === '2' ? vue2 : vue3;
+  const outDir = VUE_VERSION === '2' ? 'vue2' : 'vue3';
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    plugins: [vuePlugin()],
+    build: {
+      outDir: `./${outDir}`,
+      lib: {
+        entry: resolve(__dirname, 'src/main.ts'),
+        name: 'npm-vite-component',
+        fileName: (format) => `npm-vite-component.${format}.js`,
       },
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue"
+      rollupOptions: {
+        external: ['vue'],
+        resolve: {
+          dedupe: ['vue']
         },
-      }
-    },
-  }
-})
+        output: {
+          globals: {
+            vue: "Vue"
+          },
+        }
+      },
+    }
+  })
+}
