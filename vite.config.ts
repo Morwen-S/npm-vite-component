b53fb3b2a1995dae3cda364f7cfb78513daedc96
Svetlana Butina <s.butina@geeksoft.kz>
2022-08-26 13:38:16 +0600

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
